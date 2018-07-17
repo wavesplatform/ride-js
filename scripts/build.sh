def97bece75959426f277a7ff45a4bfaf6f6be43
Yuriy Naydenov <ebceu44@gmail.com>
2018-07-17 12:44:50 +0300

    build.sh without publish to npm

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 cd /opt/src
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
+#echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
 npm i
-npm --no-git-tag-version version $VERSION
+#npm --no-git-tag-version version $VERSION
+#npm --no-git-tag-version version prerelease
 npm run build
-npm run publish
-rm .npmrc
+#npm run publish
+#rm .npmrc
+
+

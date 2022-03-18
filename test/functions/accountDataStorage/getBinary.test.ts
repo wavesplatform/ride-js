6eaa99b1698ab4a240b078b25ef0433ee58f056d
vnikolaenko <vnikolaenko@web3tech.ru>
2022-03-18 19:22:27 +0300

    completed tests 'get byte array by address' 'get byte array by alias'

--- test/functions/accountDataStorage/getBinary.test.ts
+++ test/functions/accountDataStorage/getBinary.test.ts
@@ -1,6 +1,5 @@
 const compiler = require('../../../src');
 const scalaJsCompiler = require('../../../src/lang-opt.js');
-const {binaryValue} = require("../../testData/binary");
 
 
 describe('getBinary',  () => {
@@ -14,19 +13,30 @@ describe('getBinary',  () => {
             BinaryEntry("bin", binValue)
         ]`;
 
-    test.each([
-        [3, RideV3Result],
-        [4, GreaterV3Result],
-        [5, GreaterV3Result],
-        [44, GreaterV3Result],
-    ])('get byte array by Address', (version, scriptResult) => {
+    const testAddress = "Address(base58'')";
+    const testAlias = 'Alias("merry")';
 
-        let contract = generateContract(version, scriptResult);
+    test.each([
+        [3, RideV3Result, testAddress],
+        [4, GreaterV3Result, testAddress],
+        [5, GreaterV3Result, testAddress],
+    ])('get byte array by address', (version, scriptResult, address) => {
+        let contract = generateContract(version, scriptResult, address);
         const compiled = compiler.compile(contract);
         expect(compiled.error).toBeUndefined();
     })
 
-    let generateContract = (libVersion, caseForVersions) => {
+    test.each([
+        [3, RideV3Result, testAlias],
+        [4, GreaterV3Result, testAlias],
+        [5, GreaterV3Result, testAlias],
+    ])('get byte array by alias', (version, scriptResult, alias) => {
+        let contract = generateContract(version, scriptResult, alias);
+        const compiled = compiler.compile(contract);
+        expect(compiled.error).toBeUndefined();
+    })
+
+    let generateContract = (libVersion, caseForVersions, testData) => {
             return `
         {-# STDLIB_VERSION ${libVersion} #-}
         {-# CONTENT_TYPE DAPP #-}
@@ -34,8 +44,8 @@ describe('getBinary',  () => {
 
         @Callable(i)
         func binary() = {
-            let callerAddress = Address(base58'')
-            let binValueOrUnit = getBinary(callerAddress, "Ȣ瞱蛉㦎᠖꭛믳癚曉续")
+            let callerAddressOrAlias = ${testData}
+            let binValueOrUnit = getBinary(callerAddressOrAlias, "Ȣ瞱蛉㦎᠖꭛믳癚曉续")
             let binValue = match(binValueOrUnit) {
               case b:ByteVector => b
               case _ => throw("not binary")

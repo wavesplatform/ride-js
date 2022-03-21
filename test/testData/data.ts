8a7385cc4d63eeaf47e4d9129b96daf03a77f803
vnikolaenko <vnikolaenko@web3tech.ru>
2022-03-21 19:05:13 +0300

    refactoring data.ts

--- test/testData/data.ts
+++ test/testData/data.ts
@@ -1,69 +1,11 @@
-
 export const STDLIB_VERSION_3 = 3;
 export const STDLIB_VERSION_4 = 4;
 export const STDLIB_VERSION_5 = 5;
 export const STDLIB_INVALID_VERSION = 44;
 
-
-export const RideV3Result = `
-        let binValueOrUnit = getBinary(callerAddressOrAlias, "Ȣ瞱蛉㦎᠖꭛믳癚曉续")
-        let binValue = match(binValueOrUnit) {
-          case b:ByteVector => b
-          case _ => throw("not binary")
-        }
-        WriteSet([
-            DataEntry("binaryValue", binValue)
-        ])`;
-
-export const GreaterV3Result = `
-        let binValueOrUnit = getBinary(callerAddressOrAlias, "Ȣ瞱蛉㦎᠖꭛믳癚曉续")
-        let binValue = match(binValueOrUnit) {
-            case b:ByteVector => b
-            case _ => throw("not binary")
-        }
-        [
-            BinaryEntry("bin", binValue)
-        ]`;
-
-export const InvalidDataEntryV3Result = `
-        let binValueOrUnit = getBinary(callerAddressOrAlias, "Ȣ瞱蛉㦎᠖꭛믳癚曉续")
-        let binValue = match(binValueOrUnit) {
-            case b:ByteVector => b
-            case _ => throw("not binary")
-        }
-        WriteSet([
-            DataEntry("binaryValue")
-        ])`;
-
-export const InvalidBinaryEntryGreaterV3Result = `
-        let binValueOrUnit = getBinary(callerAddressOrAlias, "Ȣ瞱蛉㦎᠖꭛믳癚曉续")
-        let binValue = match(binValueOrUnit) {
-            case b:ByteVector => b
-            case _ => throw("not binary")
-        }
-        [
-            BinaryEntry(binValue)
-        ]`;
-
-export const InvalidGetBinaryV3Result = `
-        let binValueOrUnit = getBinary(callerAddressOrAlias)
-        let binValue = match(binValueOrUnit) {
-            case b:ByteVector => b
-            case _ => throw("not binary")
-        }
-        WriteSet([
-            DataEntry("binaryValue", binValue)
-        ])`
-
-export const InvalidGetBinaryGreaterV3Result = `
-        let binValueOrUnit = getBinary(callerAddressOrAlias)
-        let binValue = match(binValueOrUnit) {
-            case b:ByteVector => b
-            case _ => throw("not binary")
-        }
-        [
-            BinaryEntry("bin", binValue)
-        ]`
+export const defaultGetBinary = `getBinary(callerAddressOrAlias, "Ȣ瞱蛉㦎᠖꭛믳癚曉续")`;
+export const invalidGetBinaryV3 = `getBinary(callerAddressOrAlias)`;
+export const InvalidGetBinaryGreaterV3 = `getBinary(callerAddressOrAlias)`;
 
 const addressDataArray = [
     "Address(base58'')",
@@ -81,3 +23,23 @@ const aliasDataArray = [
 
 export const getRandomAddress = () => addressDataArray[Math.floor((Math.random() * addressDataArray.length))];
 export const getRandomAlias = () => aliasDataArray[Math.floor((Math.random() * aliasDataArray.length))];
+
+export const RideV3Result = `
+        WriteSet([
+            DataEntry("binaryValue", binValue)
+        ])`;
+
+export const GreaterV3Result = `
+        [
+            BinaryEntry("bin", binValue)
+        ]`;
+
+export const InvalidDataEntryV3Result = `
+        WriteSet([
+            DataEntry("binaryValue")
+        ])`;
+
+export const InvalidBinaryEntryGreaterV3Result = `
+        [
+            BinaryEntry(binValue)
+        ]`;

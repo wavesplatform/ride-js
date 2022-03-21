2216e541299495bcf378e88dfe075d19b6b82b3b
vnikolaenko <vnikolaenko@web3tech.ru>
2022-03-21 19:52:56 +0300

    edited Can't find a function overload 'getBinary'(String) scenario and add new test in getBinaryOwnData.test.ts

--- test/functions/accountDataStorage/getBinaryOwnData.test.ts
+++ test/functions/accountDataStorage/getBinaryOwnData.test.ts
@@ -1,12 +1,11 @@
 import * as data from "../../testData/data";
 
 const compiler = require('../../../src');
-const scalaJsCompiler = require('../../../src/lang-opt.js');
 
 describe('getBinaryOwnData',  () => {
 
     test.each([
-        [data.STDLIB_VERSION_5, data.GreaterV3Result, data.getRandomAddress()],
+        [data.STDLIB_VERSION_5, data.GreaterV3Result],
     ])('getting a binary from your own data', (version, scriptResult) => {
         let contract = generateContract(version, scriptResult);
         const compiled = compiler.compile(contract);
@@ -16,12 +15,22 @@ describe('getBinaryOwnData',  () => {
     test.each([
         [data.STDLIB_VERSION_3, data.RideV3Result, data.getRandomAddress()],
         [data.STDLIB_VERSION_4, data.GreaterV3Result, data.getRandomAddress()],
-    ])('getting a binary from your own data', (version, scriptResult) => {
+    ])("Can't find a function overload 'getBinary'(String)", (version, scriptResult) => {
         let contract = generateContract(version, scriptResult);
         const compiled = compiler.compile(contract);
         expect(compiled.error)
-            .toContain(`Compilation failed: [Can't find a function overload 'getBinary'(String) in`);
+            .toContain(`Compilation failed: [Can't find a function overload 'getBinary'(String)`);
     });
+
+    test.each([
+        [data.STDLIB_INVALID_VERSION, data.RideV3Result],
+        [data.STDLIB_INVALID_VERSION, data.GreaterV3Result]
+    ])("invalid lib version", (version, scriptResult) => {
+        let contract = generateContract(version, scriptResult);
+        const compiled = compiler.compile(contract);
+        expect(compiled.error)
+            .toEqual(`Illegal directive value ${data.STDLIB_INVALID_VERSION} for key STDLIB_VERSION`);
+    })
 
     const generateContract = (libVersion, caseForVersions) => {
         return `

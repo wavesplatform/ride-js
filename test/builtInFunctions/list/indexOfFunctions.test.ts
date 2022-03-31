6e799e3958f319aacd30457a12d08b05d20964c8
vnikolaenko <vnikolaenko@web3tech.ru>
2022-03-31 19:57:48 +0300

    added edit indexOfFunctions.test.ts added lastIndexOf tests

--- test/builtInFunctions/list/indexOfFunctions.test.ts
+++ test/builtInFunctions/list/indexOfFunctions.test.ts
@@ -4,10 +4,12 @@ import * as random from "../../testData/random";
 import {GenerateContractForBuiltInFunctions} from "../GenerateContractForBuiltInFunctions";
 import {checkCompileResult} from "../testResult";
 
-describe('indexOf functions',  () => {
+describe('indexOf and lastIndexOf functions',  () => {
 
     const indexOf = `indexOf(list, randomData)`;
     const invalidIndexOf = `indexOf(randomData)`;
+    const lastIndexOf = `lastIndexOf(list, randomData)`;
+    const invalidLastIndexOf = `lastIndexOf(randomData)`;
 
     const precondition = new GenerateContractForBuiltInFunctions(indexOf);
 
@@ -23,6 +25,18 @@ describe('indexOf functions',  () => {
         [data.STDLIB_VERSION_5, invalidIndexOf, random.getRandomIssuesArray(), random.getRandomAlias(), data.negativeTestType],
         // Can't find a function 'indexOf' for ride v3
         [data.STDLIB_VERSION_3, indexOf, random.getRandomStringArray(), data.stringList, data.negativeTestType],
+
+        // lastIndexOf
+        [data.STDLIB_VERSION_4, lastIndexOf, random.getRandomInt(), data.intList, data.positiveTestType],
+        [data.STDLIB_VERSION_5, lastIndexOf, random.getRandomStringArray(), data.stringList, data.positiveTestType],
+        // invalid data lastIndexOf
+        [data.STDLIB_VERSION_4, lastIndexOf, random.getRandomAlias(), data.intList, data.negativeTestType],
+        [data.STDLIB_VERSION_5, lastIndexOf, random.getRandomIssuesArray(), data.stringList, data.negativeTestType],
+        // invalid function lastIndexOf
+        [data.STDLIB_VERSION_4, invalidLastIndexOf, random.getRandomAlias(), random.getRandomStringArray(), data.negativeTestType],
+        [data.STDLIB_VERSION_5, invalidLastIndexOf, random.getRandomIssuesArray(), random.getRandomAlias(), data.negativeTestType],
+        // Can't find a function 'lastIndexOf' for ride v3
+        [data.STDLIB_VERSION_3, lastIndexOf, random.getRandomStringArray(), data.stringList, data.negativeTestType],
     ])('check ride v%i function %s compiles or failed',
         (version, testFunction, randomData, randomList, testType) => {
             const contract = precondition.generateContractForList(version, randomData, randomList, testFunction);

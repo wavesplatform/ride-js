6199744df505da9a4eed31f80a2e47d8e3c95c79
vnikolaenko <vnikolaenko@web3tech.ru>
2022-06-15 16:19:48 +0300

    added new tests STDLIB_VERSION_6, for ecrecover.test.ts

--- test/builtInFunctions/verification/ecrecover.test.ts
+++ test/builtInFunctions/verification/ecrecover.test.ts
@@ -16,12 +16,15 @@ describe('ecrecover functions',  () => {
         // ecrecover
         [data.STDLIB_VERSION_4, ecrecover, random.getRandomByteVector(), data.POSITIVE_TEST],
         [data.STDLIB_VERSION_5, ecrecover, random.getRandomByteVector(), data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, ecrecover, random.getRandomByteVector(), data.POSITIVE_TEST],
         // invalid data ecrecover
         [data.STDLIB_VERSION_4, ecrecover, random.getRandomUnion(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, ecrecover, random.getRandomAddress(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, ecrecover, random.getRandomDigestAlgorithmType(), data.NEGATIVE_TEST],
         // invalid function ecrecover
-        [data.STDLIB_VERSION_4, invalidecrecover, random.getRandomAddress(), data.NEGATIVE_TEST],
-        [data.STDLIB_VERSION_5, invalidecrecover, random.getRandomString(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_4, invalidecrecover, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_5, invalidecrecover, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, invalidecrecover, random.getRandomByteVector(), data.NEGATIVE_TEST],
         // can't find a function 'ecrecover'
         [data.STDLIB_VERSION_3, ecrecover, random.getRandomByteVector(), data.NEGATIVE_TEST],
     ])('check ride v%i function %s compiles or failed',(version, testFunction, randomData, testType) => {

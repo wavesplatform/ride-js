0b3d5fa7b719585e72f7acb0d5d6b350e8b4542b
vnikolaenko <vnikolaenko@web3tech.ru>
2022-06-15 16:17:28 +0300

    added new tests STDLIB_VERSION_6, for groth16Verify.test.ts

--- test/builtInFunctions/verification/groth16Verify.test.ts
+++ test/builtInFunctions/verification/groth16Verify.test.ts
@@ -62,163 +62,211 @@ describe('groth16Verify functions.',  () => {
         // positive groth16Verify tests
         [data.STDLIB_VERSION_4, groth16Verify, random.getRandomByteVector(), data.POSITIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify, random.getRandomByteVector(), data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify, random.getRandomByteVector(), data.POSITIVE_TEST],
         // invalid byteVector in groth16Verify
         [data.STDLIB_VERSION_4, groth16Verify, random.getRandomAlias(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify, random.getRandomUnion(), data.NEGATIVE_TEST],
         // invalid function invalidGroth16Verify
         [data.STDLIB_VERSION_4, invalidGroth16Verify, random.getRandomByteVector(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, invalidGroth16Verify, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, invalidGroth16Verify, random.getRandomByteVector(), data.NEGATIVE_TEST],
 
         // positive groth16Verify_1inputs tests
         [data.STDLIB_VERSION_4, groth16Verify_1inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_1inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_1inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         // invalid byteVector in groth16Verify_1inputs
         [data.STDLIB_VERSION_4, groth16Verify_1inputs, random.getRandomAlias(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_1inputs, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_1inputs, random.getRandomAddress(), data.NEGATIVE_TEST],
         // invalid function invalidGroth16Verify_1inputs
         [data.STDLIB_VERSION_4, invalidGroth16Verify_1inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, invalidGroth16Verify_1inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, invalidGroth16Verify_1inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
 
         // positive groth16Verify_2inputs tests
         [data.STDLIB_VERSION_4, groth16Verify_2inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_2inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_2inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         // invalid byteVector in groth16Verify_2inputs
         [data.STDLIB_VERSION_4, groth16Verify_2inputs, random.getRandomAlias(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_2inputs, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_2inputs, random.getRandomDigestAlgorithmType(), data.NEGATIVE_TEST],
         // invalid function invalidgroth16Verify_2inputs
         [data.STDLIB_VERSION_4, invalidGroth16Verify_2inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, invalidGroth16Verify_2inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, invalidGroth16Verify_2inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
 
         // positive groth16Verify_3inputs tests
         [data.STDLIB_VERSION_4, groth16Verify_3inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_3inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_3inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         // invalid byteVector in groth16Verify_3inputs
         [data.STDLIB_VERSION_4, groth16Verify_3inputs, random.getRandomAlias(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_3inputs, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_3inputs, random.getRandomString(), data.NEGATIVE_TEST],
         // invalid function invalidgroth16Verify_3inputs
         [data.STDLIB_VERSION_4, invalidGroth16Verify_3inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, invalidGroth16Verify_3inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, invalidGroth16Verify_3inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
 
         // positive groth16Verify_4inputs tests
         [data.STDLIB_VERSION_4, groth16Verify_4inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_4inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_4inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         // invalid byteVector in groth16Verify_4inputs
         [data.STDLIB_VERSION_4, groth16Verify_4inputs, random.getRandomAlias(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_4inputs, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_4inputs, random.getRandomAddress(), data.NEGATIVE_TEST],
         // invalid function invalidGroth16Verify_4inputs
         [data.STDLIB_VERSION_4, invalidGroth16Verify_4inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, invalidGroth16Verify_4inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, invalidGroth16Verify_4inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
 
         // positive groth16Verify_5inputs tests
         [data.STDLIB_VERSION_4, groth16Verify_5inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_5inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_5inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         // invalid byteVector in groth16Verify_5inputs
         [data.STDLIB_VERSION_4, groth16Verify_5inputs, random.getRandomAlias(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_5inputs, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_5inputs, random.getRandomAlias(), data.NEGATIVE_TEST],
         // invalid function invalidGroth16Verify_5inputs
         [data.STDLIB_VERSION_4, invalidGroth16Verify_5inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, invalidGroth16Verify_5inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, invalidGroth16Verify_5inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
 
         // positive groth16Verify_6inputs tests
         [data.STDLIB_VERSION_4, groth16Verify_6inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_6inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_6inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         // invalid byteVector in groth16Verify_6inputs
         [data.STDLIB_VERSION_4, groth16Verify_6inputs, random.getRandomAlias(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_6inputs, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_6inputs, random.getRandomAddress(), data.NEGATIVE_TEST],
         // invalid function invalidGroth16Verify_6inputs
         [data.STDLIB_VERSION_4, invalidGroth16Verify_6inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, invalidGroth16Verify_6inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, invalidGroth16Verify_6inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
 
         // positive groth16Verify_7inputs tests
         [data.STDLIB_VERSION_4, groth16Verify_7inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_7inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_7inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         // invalid byteVector in groth16Verify_7inputs
         [data.STDLIB_VERSION_4, groth16Verify_7inputs, random.getRandomAlias(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_7inputs, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_7inputs, random.getRandomIssue(), data.NEGATIVE_TEST],
         // invalid function invalidGroth16Verify_7inputs
         [data.STDLIB_VERSION_4, invalidGroth16Verify_7inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, invalidGroth16Verify_7inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, invalidGroth16Verify_7inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
 
         // positive groth16Verify_8inputs tests
         [data.STDLIB_VERSION_4, groth16Verify_8inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_8inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_8inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         // invalid byteVector in groth16Verify_8inputs
         [data.STDLIB_VERSION_4, groth16Verify_8inputs, random.getRandomAlias(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_8inputs, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_8inputs, random.getRandomDigestAlgorithmType(), data.NEGATIVE_TEST],
         // invalid function invalidGroth16Verify_8inputs
         [data.STDLIB_VERSION_4, invalidGroth16Verify_8inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, invalidGroth16Verify_8inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, invalidGroth16Verify_8inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
 
         // positive groth16Verify_9inputs tests
         [data.STDLIB_VERSION_4, groth16Verify_9inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_9inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_9inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         // invalid byteVector in groth16Verify_9inputs
         [data.STDLIB_VERSION_4, groth16Verify_9inputs, random.getRandomAlias(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_9inputs, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_9inputs, random.getRandomString(), data.NEGATIVE_TEST],
         // invalid function invalidGroth16Verify_9inputs
         [data.STDLIB_VERSION_4, invalidGroth16Verify_9inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, invalidGroth16Verify_9inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, invalidGroth16Verify_9inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
 
 
         // positive groth16Verify_10inputs tests
         [data.STDLIB_VERSION_4, groth16Verify_10inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_10inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_10inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         // invalid byteVector in groth16Verify_10inputs
         [data.STDLIB_VERSION_4, groth16Verify_10inputs, random.getRandomAlias(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_10inputs, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_10inputs, random.getRandomInt(), data.NEGATIVE_TEST],
         // invalid function invalidGroth16Verify_10inputs
         [data.STDLIB_VERSION_4, invalidGroth16Verify_10inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, invalidGroth16Verify_10inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, invalidGroth16Verify_10inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
 
         // positive groth16Verify_11inputs tests
         [data.STDLIB_VERSION_4, groth16Verify_11inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_11inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_11inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         // invalid byteVector in groth16Verify_11inputs
         [data.STDLIB_VERSION_4, groth16Verify_11inputs, random.getRandomAlias(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_11inputs, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_11inputs, random.getRandomUnion(), data.NEGATIVE_TEST],
         // invalid function invalidGroth16Verify_11inputs
         [data.STDLIB_VERSION_4, invalidGroth16Verify_11inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, invalidGroth16Verify_11inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, invalidGroth16Verify_11inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
 
         // positive groth16Verify_12inputs tests
         [data.STDLIB_VERSION_4, groth16Verify_12inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_12inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_12inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         // invalid byteVector in groth16Verify_12inputs
         [data.STDLIB_VERSION_4, groth16Verify_12inputs, random.getRandomAlias(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_12inputs, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_12inputs, random.getRandomAddress(), data.NEGATIVE_TEST],
         // invalid function invalidGroth16Verify_12inputs
         [data.STDLIB_VERSION_4, invalidGroth16Verify_12inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, invalidGroth16Verify_12inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, invalidGroth16Verify_12inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
 
         // positive groth16Verify_13inputs tests
         [data.STDLIB_VERSION_4, groth16Verify_13inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_13inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_13inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         // invalid byteVector in groth16Verify_13inputs
         [data.STDLIB_VERSION_4, groth16Verify_13inputs, random.getRandomAlias(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_13inputs, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_13inputs, random.getRandomDigestAlgorithmType(), data.NEGATIVE_TEST],
         // invalid function invalidGroth16Verify_13inputs
         [data.STDLIB_VERSION_4, invalidGroth16Verify_13inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, invalidGroth16Verify_13inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, invalidGroth16Verify_13inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
 
         // positive groth16Verify_14inputs tests
         [data.STDLIB_VERSION_4, groth16Verify_14inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_14inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_14inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         // invalid byteVector in groth16Verify_14inputs
         [data.STDLIB_VERSION_4, groth16Verify_14inputs, random.getRandomAlias(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_14inputs, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_14inputs, random.getRandomIssue(), data.NEGATIVE_TEST],
         // invalid function invalidGroth16Verify_14inputs
         [data.STDLIB_VERSION_4, invalidGroth16Verify_14inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, invalidGroth16Verify_14inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, invalidGroth16Verify_14inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
 
         // positive groth16Verify_15inputs tests
         [data.STDLIB_VERSION_4, groth16Verify_15inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_15inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_15inputs, random.getRandomByteVector(), data.POSITIVE_TEST],
         // invalid byteVector in groth16Verify_15inputs
         [data.STDLIB_VERSION_4, groth16Verify_15inputs, random.getRandomAlias(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, groth16Verify_15inputs, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, groth16Verify_15inputs, random.getRandomUnion(), data.NEGATIVE_TEST],
         // invalid function invalidGroth16Verify_15inputs
         [data.STDLIB_VERSION_4, invalidGroth16Verify_15inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, invalidGroth16Verify_15inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, invalidGroth16Verify_15inputs, random.getRandomByteVector(), data.NEGATIVE_TEST],
 
         // Can't find a function 'groth16Verify' for ride v3
         [data.STDLIB_VERSION_3, groth16Verify, random.getRandomByteVector(), data.NEGATIVE_TEST],

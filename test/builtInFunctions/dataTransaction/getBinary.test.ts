629b17827a7b40d3a945b3b32fc443a7fc3189d1
vnikolaenko <vnikolaenko@web3tech.ru>
2022-06-23 17:18:05 +0300

    added data transaction functions tests, with arguments before function

--- test/builtInFunctions/dataTransaction/getBinary.test.ts
+++ test/builtInFunctions/dataTransaction/getBinary.test.ts
@@ -3,12 +3,18 @@ import * as random from "../../testData/random";
 import {GenerateContractForBuiltInFunctions} from "../GenerateContractForBuiltInFunctions";
 import {checkCompileResult} from "../testResult";
 
-describe('dataTransaction - getBinary / getBinaryValue',  () => {
-
+describe('dataTransaction - getBinary / getBinaryValue', () => {
     const getBinaryKey = `getBinary(callerTestData, "key")`;
     const getBinaryIndex = `getBinary(callerTestData, ${random.getRandomInt()})`;
     const getBinaryValueKey = `getBinaryValue(callerTestData, "key")`;
     const getBinaryValueIndex = `getBinaryValue(callerTestData, ${random.getRandomInt()})`;
+    const invalidGetBinaryKey = `getBinary()`;
+
+    const getBinaryKeyArgBeforeFunc = `callerTestData.getBinary("key")`;
+    const getBinaryIndexArgBeforeFunc = `callerTestData.getBinary(${random.getRandomInt()})`;
+    const getBinaryValueKeyArgBeforeFunc = `callerTestData.getBinaryValue("key")`;
+    const getBinaryValueIndexArgBeforeFunc = `callerTestData.getBinaryValue(${random.getRandomInt()})`;
+    const invalidGetBinaryArgBeforeFunc = `callerTestData.getBinary()`;
 
     const precondition = new GenerateContractForBuiltInFunctions(getBinaryKey);
     precondition.setData("ByteVector");
@@ -60,9 +66,72 @@ describe('dataTransaction - getBinary / getBinaryValue',  () => {
         [data.STDLIB_VERSION_4, getBinaryValueIndex, data.GreaterV3ResultBinaryEntry, random.getRandomByteVector(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, getBinaryValueIndex, data.GreaterV3ResultBinaryEntry, random.getRandomAddress(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_6, getBinaryValueIndex, data.GreaterV3ResultBinaryEntry, random.getRandomAddress(), data.NEGATIVE_TEST],
+        // invalid function
+        [data.STDLIB_VERSION_3, invalidGetBinaryKey, data.RideV3Result, data.dataEntryForTests, data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_4, invalidGetBinaryKey, data.GreaterV3ResultBinaryEntry, data.binaryEntryForTests, data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_4, invalidGetBinaryKey, data.GreaterV3ResultBinaryEntry, data.integerEntryForTests, data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_5, invalidGetBinaryKey, data.GreaterV3ResultBinaryEntry, data.stringEntryForTests, data.NEGATIVE_TEST],
     ])('check ride v%i function %s compiles or failed',
         (version, testFunction, caseForVersions, callerTestData, testType) => {
-        const contract = precondition.generateContractFromMatchingAndCase(version, caseForVersions, callerTestData, testFunction);
-        checkCompileResult(contract, testType);
-    });
+            const contract = precondition.generateContractFromMatchingAndCase(version, caseForVersions, callerTestData, testFunction);
+            checkCompileResult(contract, testType);
+        });
+
+    test.each([
+        // getBinaryKey - callerTestData.getBinary("key")
+        [data.STDLIB_VERSION_3, getBinaryKeyArgBeforeFunc, data.RideV3Result, data.dataEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_4, getBinaryKeyArgBeforeFunc, data.GreaterV3ResultBinaryEntry, data.binaryEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_4, getBinaryKeyArgBeforeFunc, data.GreaterV3ResultBinaryEntry, data.integerEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_5, getBinaryKeyArgBeforeFunc, data.GreaterV3ResultBinaryEntry, data.stringEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_5, getBinaryKeyArgBeforeFunc, data.GreaterV3ResultBinaryEntry, data.booleanEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, getBinaryKeyArgBeforeFunc, data.GreaterV3ResultBinaryEntry, data.binaryEntryForTests, data.POSITIVE_TEST],
+        // getBinaryKeyArgBeforeFunc - invalid data for List[].getBinary(key)
+        [data.STDLIB_VERSION_3, getBinaryKeyArgBeforeFunc, data.RideV3Result, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_4, getBinaryKeyArgBeforeFunc, data.GreaterV3ResultBinaryEntry, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_5, getBinaryKeyArgBeforeFunc, data.GreaterV3ResultBinaryEntry, random.getRandomIssue(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, getBinaryKeyArgBeforeFunc, data.GreaterV3ResultBinaryEntry, random.getRandomIssue(), data.NEGATIVE_TEST],
+        // getBinaryIndexArgBeforeFunc - List[].getBinary(Int)
+        [data.STDLIB_VERSION_3, getBinaryIndexArgBeforeFunc, data.RideV3Result, data.dataEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_4, getBinaryIndexArgBeforeFunc, data.GreaterV3ResultBinaryEntry, data.binaryEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_4, getBinaryIndexArgBeforeFunc, data.GreaterV3ResultBinaryEntry, data.integerEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_5, getBinaryIndexArgBeforeFunc, data.GreaterV3ResultBinaryEntry, data.stringEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, getBinaryIndexArgBeforeFunc, data.GreaterV3ResultBinaryEntry, data.booleanEntryForTests, data.POSITIVE_TEST],
+        // getBinaryIndexArgBeforeFunc - invalid data for List[].getBinary(Int)
+        [data.STDLIB_VERSION_3, getBinaryIndexArgBeforeFunc, data.RideV3Result, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_4, getBinaryIndexArgBeforeFunc, data.GreaterV3ResultBinaryEntry, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_5, getBinaryIndexArgBeforeFunc, data.GreaterV3ResultBinaryEntry, random.getRandomAddress(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, getBinaryIndexArgBeforeFunc, data.GreaterV3ResultBinaryEntry, random.getRandomAddress(), data.NEGATIVE_TEST],
+
+        // getBinaryValueKeyArgBeforeFunc - callerTestData.getBinaryValue("key")
+        [data.STDLIB_VERSION_3, getBinaryValueKeyArgBeforeFunc, data.RideV3Result, data.dataEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_4, getBinaryValueKeyArgBeforeFunc, data.GreaterV3ResultBinaryEntry, data.binaryEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_4, getBinaryValueKeyArgBeforeFunc, data.GreaterV3ResultBinaryEntry, data.integerEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_5, getBinaryValueKeyArgBeforeFunc, data.GreaterV3ResultBinaryEntry, data.stringEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, getBinaryValueKeyArgBeforeFunc, data.GreaterV3ResultBinaryEntry, data.booleanEntryForTests, data.POSITIVE_TEST],
+        // getBinaryValueKeyArgBeforeFunc - invalid data for List[].getBinaryValueKey(key)
+        [data.STDLIB_VERSION_3, getBinaryValueKeyArgBeforeFunc, data.RideV3Result, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_4, getBinaryValueKeyArgBeforeFunc, data.GreaterV3ResultBinaryEntry, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_5, getBinaryValueKeyArgBeforeFunc, data.GreaterV3ResultBinaryEntry, random.getRandomIssue(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, getBinaryValueKeyArgBeforeFunc, data.GreaterV3ResultBinaryEntry, random.getRandomIssue(), data.NEGATIVE_TEST],
+        // getBinaryValueIndexArgBeforeFunc - List[].getBinaryValue(Int)
+        [data.STDLIB_VERSION_3, getBinaryValueIndexArgBeforeFunc, data.RideV3Result, data.dataEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_4, getBinaryValueIndexArgBeforeFunc, data.GreaterV3ResultBinaryEntry, data.binaryEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_4, getBinaryValueIndexArgBeforeFunc, data.GreaterV3ResultBinaryEntry, data.integerEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_5, getBinaryValueIndexArgBeforeFunc, data.GreaterV3ResultBinaryEntry, data.stringEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, getBinaryValueIndexArgBeforeFunc, data.GreaterV3ResultBinaryEntry, data.booleanEntryForTests, data.POSITIVE_TEST],
+        // getBinaryValueIndexArgBeforeFunc - invalid data for List[].getBinaryValue(Int)
+        [data.STDLIB_VERSION_3, getBinaryValueIndexArgBeforeFunc, data.RideV3Result, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_4, getBinaryValueIndexArgBeforeFunc, data.GreaterV3ResultBinaryEntry, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_5, getBinaryValueIndexArgBeforeFunc, data.GreaterV3ResultBinaryEntry, random.getRandomAddress(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, getBinaryValueIndexArgBeforeFunc, data.GreaterV3ResultBinaryEntry, random.getRandomAddress(), data.NEGATIVE_TEST],
+        // invalid function
+        [data.STDLIB_VERSION_3, invalidGetBinaryArgBeforeFunc, data.RideV3Result, data.dataEntryForTests, data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_4, invalidGetBinaryArgBeforeFunc, data.GreaterV3ResultBinaryEntry, data.binaryEntryForTests, data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_4, invalidGetBinaryArgBeforeFunc, data.GreaterV3ResultBinaryEntry, data.integerEntryForTests, data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_5, invalidGetBinaryArgBeforeFunc, data.GreaterV3ResultBinaryEntry, data.stringEntryForTests, data.NEGATIVE_TEST],
+    ])('check ride v%i function (arg first) %s compiles or failed',
+        (version, testFunction, caseForVersions, callerTestData, testType) => {
+            const contract = precondition.generateContractFromMatchingAndCase(version, caseForVersions, callerTestData, testFunction);
+            checkCompileResult(contract, testType);
+        });
 });
--- test/builtInFunctions/dataTransaction/getString.test.ts
+++ test/builtInFunctions/dataTransaction/getString.test.ts
@@ -3,12 +3,18 @@ import * as random from "../../testData/random";
 import {GenerateContractForBuiltInFunctions} from "../GenerateContractForBuiltInFunctions";
 import {checkCompileResult} from "../testResult";
 
-describe('dataTransaction - getString / getStringValue',  () => {
-
+describe('dataTransaction - getString / getStringValue', () => {
     const getStringKey = `getString(callerTestData, "key")`;
     const getStringIndex = `getString(callerTestData, ${random.getRandomInt()})`;
     const getStringValueKey = `getStringValue(callerTestData, "key")`;
     const getStringValueIndex = `getStringValue(callerTestData, ${random.getRandomInt()})`;
+    const invalidGetStringKey = `getString()`;
+
+    const getStringKeyArgBeforeFunc = `callerTestData.getString("key")`;
+    const getStringIndexArgBeforeFunc = `callerTestData.getString(${random.getRandomInt()})`;
+    const getStringValueKeyArgBeforeFunc = `callerTestData.getStringValue("key")`;
+    const getStringValueIndexArgBeforeFunc = `callerTestData.getStringValue(${random.getRandomInt()})`;
+    const invalidGetStringArgBeforeFunc = `callerTestData.getString()`;
 
     const precondition = new GenerateContractForBuiltInFunctions(getStringKey);
     precondition.setData("String");
@@ -19,7 +25,8 @@ describe('dataTransaction - getString / getStringValue',  () => {
         [data.STDLIB_VERSION_4, getStringKey, data.GreaterV3ResultStringEntry, data.binaryEntryForTests, data.POSITIVE_TEST],
         [data.STDLIB_VERSION_4, getStringKey, data.GreaterV3ResultStringEntry, data.integerEntryForTests, data.POSITIVE_TEST],
         [data.STDLIB_VERSION_5, getStringKey, data.GreaterV3ResultStringEntry, data.stringEntryForTests, data.POSITIVE_TEST],
-        [data.STDLIB_VERSION_6, getStringKey, data.GreaterV3ResultStringEntry, data.booleanEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_5, getStringKey, data.GreaterV3ResultStringEntry, data.booleanEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, getStringKey, data.GreaterV3ResultStringEntry, data.binaryEntryForTests, data.POSITIVE_TEST],
         // getStringKey - invalid data for getString(List[], key)
         [data.STDLIB_VERSION_3, getStringKey, data.RideV3Result, random.getRandomInt(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_4, getStringKey, data.GreaterV3ResultStringEntry, random.getRandomByteVector(), data.NEGATIVE_TEST],
@@ -59,9 +66,72 @@ describe('dataTransaction - getString / getStringValue',  () => {
         [data.STDLIB_VERSION_4, getStringValueIndex, data.GreaterV3ResultStringEntry, random.getRandomByteVector(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_5, getStringValueIndex, data.GreaterV3ResultStringEntry, random.getRandomAddress(), data.NEGATIVE_TEST],
         [data.STDLIB_VERSION_6, getStringValueIndex, data.GreaterV3ResultStringEntry, random.getRandomAddress(), data.NEGATIVE_TEST],
+        // invalid function
+        [data.STDLIB_VERSION_3, invalidGetStringKey, data.RideV3Result, data.dataEntryForTests, data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_4, invalidGetStringKey, data.GreaterV3ResultStringEntry, data.binaryEntryForTests, data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_4, invalidGetStringKey, data.GreaterV3ResultStringEntry, data.integerEntryForTests, data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_5, invalidGetStringKey, data.GreaterV3ResultStringEntry, data.stringEntryForTests, data.NEGATIVE_TEST],
     ])('check ride v%i function %s compiles or failed',
         (version, testFunction, caseForVersions, callerTestData, testType) => {
             const contract = precondition.generateContractFromMatchingAndCase(version, caseForVersions, callerTestData, testFunction);
             checkCompileResult(contract, testType);
-    });
+        });
+
+    test.each([
+        // getStringKey - callerTestData.getString("key")
+        [data.STDLIB_VERSION_3, getStringKeyArgBeforeFunc, data.RideV3Result, data.dataEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_4, getStringKeyArgBeforeFunc, data.GreaterV3ResultStringEntry, data.binaryEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_4, getStringKeyArgBeforeFunc, data.GreaterV3ResultStringEntry, data.integerEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_5, getStringKeyArgBeforeFunc, data.GreaterV3ResultStringEntry, data.stringEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_5, getStringKeyArgBeforeFunc, data.GreaterV3ResultStringEntry, data.booleanEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, getStringKeyArgBeforeFunc, data.GreaterV3ResultStringEntry, data.binaryEntryForTests, data.POSITIVE_TEST],
+        // getStringKeyArgBeforeFunc - invalid data for List[].getString(key)
+        [data.STDLIB_VERSION_3, getStringKeyArgBeforeFunc, data.RideV3Result, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_4, getStringKeyArgBeforeFunc, data.GreaterV3ResultStringEntry, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_5, getStringKeyArgBeforeFunc, data.GreaterV3ResultStringEntry, random.getRandomIssue(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, getStringKeyArgBeforeFunc, data.GreaterV3ResultStringEntry, random.getRandomIssue(), data.NEGATIVE_TEST],
+        // getStringIndexArgBeforeFunc - List[].getString(Int)
+        [data.STDLIB_VERSION_3, getStringIndexArgBeforeFunc, data.RideV3Result, data.dataEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_4, getStringIndexArgBeforeFunc, data.GreaterV3ResultStringEntry, data.binaryEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_4, getStringIndexArgBeforeFunc, data.GreaterV3ResultStringEntry, data.integerEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_5, getStringIndexArgBeforeFunc, data.GreaterV3ResultStringEntry, data.stringEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, getStringIndexArgBeforeFunc, data.GreaterV3ResultStringEntry, data.booleanEntryForTests, data.POSITIVE_TEST],
+        // getStringIndexArgBeforeFunc - invalid data for List[].getString(Int)
+        [data.STDLIB_VERSION_3, getStringIndexArgBeforeFunc, data.RideV3Result, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_4, getStringIndexArgBeforeFunc, data.GreaterV3ResultStringEntry, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_5, getStringIndexArgBeforeFunc, data.GreaterV3ResultStringEntry, random.getRandomAddress(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, getStringIndexArgBeforeFunc, data.GreaterV3ResultStringEntry, random.getRandomAddress(), data.NEGATIVE_TEST],
+
+        // getStringValueKeyArgBeforeFunc - callerTestData.getStringValue("key")
+        [data.STDLIB_VERSION_3, getStringValueKeyArgBeforeFunc, data.RideV3Result, data.dataEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_4, getStringValueKeyArgBeforeFunc, data.GreaterV3ResultStringEntry, data.binaryEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_4, getStringValueKeyArgBeforeFunc, data.GreaterV3ResultStringEntry, data.integerEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_5, getStringValueKeyArgBeforeFunc, data.GreaterV3ResultStringEntry, data.stringEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, getStringValueKeyArgBeforeFunc, data.GreaterV3ResultStringEntry, data.booleanEntryForTests, data.POSITIVE_TEST],
+        // getStringValueKeyArgBeforeFunc - invalid data for List[].getStringValueKey(key)
+        [data.STDLIB_VERSION_3, getStringValueKeyArgBeforeFunc, data.RideV3Result, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_4, getStringValueKeyArgBeforeFunc, data.GreaterV3ResultStringEntry, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_5, getStringValueKeyArgBeforeFunc, data.GreaterV3ResultStringEntry, random.getRandomIssue(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, getStringValueKeyArgBeforeFunc, data.GreaterV3ResultStringEntry, random.getRandomIssue(), data.NEGATIVE_TEST],
+        // getStringValueIndexArgBeforeFunc - List[].getStringValue(Int)
+        [data.STDLIB_VERSION_3, getStringValueIndexArgBeforeFunc, data.RideV3Result, data.dataEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_4, getStringValueIndexArgBeforeFunc, data.GreaterV3ResultStringEntry, data.binaryEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_4, getStringValueIndexArgBeforeFunc, data.GreaterV3ResultStringEntry, data.integerEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_5, getStringValueIndexArgBeforeFunc, data.GreaterV3ResultStringEntry, data.stringEntryForTests, data.POSITIVE_TEST],
+        [data.STDLIB_VERSION_6, getStringValueIndexArgBeforeFunc, data.GreaterV3ResultStringEntry, data.booleanEntryForTests, data.POSITIVE_TEST],
+        // getStringValueIndexArgBeforeFunc - invalid data for List[].getStringValue(Int)
+        [data.STDLIB_VERSION_3, getStringValueIndexArgBeforeFunc, data.RideV3Result, random.getRandomInt(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_4, getStringValueIndexArgBeforeFunc, data.GreaterV3ResultStringEntry, random.getRandomByteVector(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_5, getStringValueIndexArgBeforeFunc, data.GreaterV3ResultStringEntry, random.getRandomAddress(), data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_6, getStringValueIndexArgBeforeFunc, data.GreaterV3ResultStringEntry, random.getRandomAddress(), data.NEGATIVE_TEST],
+        // invalid function
+        [data.STDLIB_VERSION_3, invalidGetStringArgBeforeFunc, data.RideV3Result, data.dataEntryForTests, data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_4, invalidGetStringArgBeforeFunc, data.GreaterV3ResultStringEntry, data.binaryEntryForTests, data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_4, invalidGetStringArgBeforeFunc, data.GreaterV3ResultStringEntry, data.integerEntryForTests, data.NEGATIVE_TEST],
+        [data.STDLIB_VERSION_5, invalidGetStringArgBeforeFunc, data.GreaterV3ResultStringEntry, data.stringEntryForTests, data.NEGATIVE_TEST],
+    ])('check ride v%i function (arg first) %s compiles or failed',
+        (version, testFunction, caseForVersions, callerTestData, testType) => {
+            const contract = precondition.generateContractFromMatchingAndCase(version, caseForVersions, callerTestData, testFunction);
+            checkCompileResult(contract, testType);
+        });
 });

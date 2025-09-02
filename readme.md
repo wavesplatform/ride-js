d0977714ea781b61ca1eaf1edf4f1df317d7ce02
Sergey Nazarov <phearnot@renee.ru>
2025-09-02 19:26:11 +0300

    wip

--- readme.md
+++ readme.md
@@ -1,23 +1,5 @@
 # Ride-js
 Javascript wrapper for [ride language](https://docs.wavesplatform.com/en/smart-contracts/ride-language/ride-language.html) compiler
 
-This repository also contains version of the compiler build with [scala-js](https://www.scala-js.org/)
-### To update compiler version in this wrapper:
-* Clone [waves](https://github.com/wavesplatform/waves) project
-* Checkout to branch of your choice to build new compiler version
-* Build js compiler using sbt task 
-```sbt
-sbt langJS/fullOptJS
-sbt replJS/fullOptJS
-```
-* From `waves` project copy ```./lang/js/target/lang-opt.js``` to ```./src```  of `ride-js`
-* From `waves` project copy ```./repl/js/target/repl-opt.js``` to ```./src```  of `ride-js`
-
-# Workaround
-If there is error with compiling try
-```sbt
-sbt clean && sbt lang/clean
-```
-
 ### Tasks
 * `npm run build` builds minified version that can be included as script tag. Add RideJS variable to global scope
--- test/compiler.test.ts
+++ test/compiler.test.ts
@@ -133,8 +133,23 @@ func bar() = WriteSet([])`;
 
     test.only('rsa verify', async function () {
         const {evaluate} = compiler.repl();
-        const pk = `let pk = fromBase64String("MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAkDg8m0bCDX7fTbBlHZm+BZIHVOfC2I4klRbjSqwFi/eCdfhGjYRYvu/frpSO0LIm0beKOUvwat6DY4dEhNt2PW3UeQvT2udRQ9VBcpwaJlLreCr837sn4fa9UG9FQFaGofSww1O9eBBjwMXeZr1jOzR9RBIwoL1TQkIkZGaDXRltEaMxtNnzotPfF3vGIZZuZX4CjiitHaSC0zlmQrEL3BDqqoLwo3jq8U3Zz8XUMyQElwufGRbZqdFCeiIs/EoHiJm8q8CVExRoxB0H/vE2uDFK/OXLGTgfwnDlrCa/qGt9Zsb8raUSz9IIHx72XB+kOXTt/GOuW7x2dJvTJIqKTwIDAQAB")`
-        const msg = `let msg = fromBase64String("REIiN2hDQUxIJVQzdk1zQSpXclRRelExVWd+YGQoOyx0KHduPzFmcU8zUWosWiA7aFloOWplclAxPCU=")`
+        const pk = `let pk = fromBase64String("MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAt5IE7IAnSq7uK8FknxfEm2OtPvFOQlVy4F9arLp0PmhIRkDMpk7nWu3aNn6NBYX4kiigOLBhRDwNAZTJXnCjS8FQ/trZRo7oANiCX9kKwJZKQQCjLS0KSRWQWunDF7l9EUhTwb3QzhdSvYJLy3lOk90ZPB+36YvHooFx8oLIJimJhgbPXL95Yk6i+wh32Zhda616+9q/EftA5I4emJZRFLareSXM/MR03IFjYdh4S7LH+OPr94IQY/26Pt5HmS0X4W500HjxEp1vF8Irx3GYiF6Abk7JK5Gyf6W8ApEfAofj0s8qfLfHhH4JHg/QwW4NSd1NrhRMov2H7v31BVsRgwIDAQAB")`
+        const msg = `let msg = fromBase64String("aGVsbG8gd29ybGQ=")`
+
+        const signatures = [
+            {alg:"NOALG","sig":"eSYkQMhRYhSVCoJoulhT7Q5esRGMJS/9ztMjDbvCAI4zlZnuIJkFfT7BS5rvwQ1vAeBnWX1lmi+BrbJ8/6elUsavEd6ZTg9GHkKIVwXeh3LqnyChmRYRzwjk2s6+WD9S9M3YUpgoNL1e2rwegA7sE9QZ4FpZyLZAk0PW0vOjIC33DSB1M6nv0vSzlFkwj3EHf8/vrYprnvtsZYWjWhmxV3oh8dYdE5lNRTvnYRSfQCq9NpRNt/ErRphTTwhk78GcYptTvR6sdIQNGvncwDOUHVoxZF1+HmyxPuQztE9xPVeVHuo8lezLqmOh84Li48WfvjYNBfEqZNTKyht0CAegDg=="},
+            {alg:"MD5",sig:"kHOfaGcMkCSaVBwwyg/yy0IrYM3wntSh6/AxfdXDWyGgtxlOoeMi45Btw71hk8a+M2xCQ4FgTu2s3lWbYSTk3qf+hDOgRqOoKDoehAwtMC+DH/d/kcgArtnf9g13D4gbpWrcjb5M9Q6fNcPUfZDT6U13exh6rbjKGdpNCun2DqHzpUUUn96Jkc7XYXQwyEN8IU0J5Ez38dqlyDEvuRykjz/ABd/tQxp1IivVZ9OgAJttVlfAmmYCHPSSQfXAQk1w4fLremHqolQJGZzDCfKkqucni/BdWnMXJikI63y0u1++3Jnipb93PrfmLlGxWinwybD3O9oKiZ2SigHy5/t6eA=="},
+            {alg:"SHA1",sig:"r06p9Zmdx+tJQYS2rDq4XLY4TZFofovPsq94gAkI4yCCBIQg7I+pkuixOONisnJZmItUWF9vvo+AGBTSDHDQWcTiVWLXD49ynlEG0GOS/W9zsT7KWYRwaaulXL7PFHSDC87OcuS2n0KEivM3K20QhcC+X/cNR5c6vJ2nmuAE/3xU1qlnMm/bUQYicuQOD0gKLb1BuVqFAZ/KQfKiuzdOoX9Pkg135qAygGSRRFIhJ67kb2lRpqeFS4FgHqc6Cm3oINDxx9MZTZ/DZnaqAByABbAIQhPrZeekg3Ysj8FoOvoxZGpvzb5cbjO6akV+aHnqOcOKksl/h5EKy2BjfNJpWA=="},
+            // {alg:"SHA224",sig:"ZYRap9zdQ8SUVHIJky4dXyen/TofgDBjLazxIs7Piz6a2CLyHL7cVZLBLbikIzP4rk9o+ZuzOdmaUCqfEWXzj97om6BdbvgDaXrv5IM92Q2Bd3EH1VoEpYQtDmAJOyJV9VSGkjMlOosR31jppQ0UUKHYkqo9dxnkLpTv6XkLtQNUP7NLazxP2KbT4gnffXf2opXvUNWpiiywVs8vilQmkL1EocjOopf9rclWcH2LkcXM3NHrk7wqKyDsvclh1LogWsNiNXmcnvKrpD5AEOxGw3k0F0tre/eFP915b4kBbozfs4RRwAqTkrEzNbfYp/Id4C8WB8a5MObUQPwglHu1Og=="},
+            {alg:"SHA256",sig:"AD/Q/AjmUUYlEec3LbBBNA+R8A+hlHyZoQs5BUuawJRqF0ROeJZLXktSegO8q6v+/W5yBRwuSbVSTyb/vtvvM0Qr8ayhKNpcF0unYtILO6g0farJGMU21Ne0I5nNknRveLmXY6itatba+3OU510HtZWmXo+y8qXNEp2VRI6Qpz2hzl1xt9qg8/psuAPNpk1OpFqShyh9lHNkwFQYJ2lDnoMkDWFof4SojTYAvL01sRtFGwjMb+K71QvGH0RX1FdGId/tubmFQRfMJuVpZkGgyZ/8PD245MkxbIJCAKraFst5n0Wi6wx++CT31fU4qLGYdaW3MW3h9zPPUV9RLJfeDQ=="},
+            {alg:"SHA384",sig:"RXjxfAhg4f4G/UkRoyxnCTTKkME2ceTjQJJEgtL7O19srM3yFFqY2cqVFIFpOJBEQ+4KdncGZeQZmsHQrXfq5U8O9rZi9ugURKnG07l/hjvU6UqUJfWYCY31rEQUpURcH00GNmIUu8wQwFmTJT1LHao0tByAP01N7VJ2JSTzHbpb/buQh27AyIKoH8JibT5qQMS8LAf/m+Jxr0nGFyLd0KwV+YAs1TUfmOPSR15VeyN3qmL5PVB4j5tHXpa0fVq9ALVrvKyIuQDVX0w1L6KMcy+Y6tz3eBZrs+V7Jf5dEYe2+JXTxnAZU1c55DMIBCvPly76wYpL8w8XUA5WyJCw8w=="},
+            {alg:"SHA512",sig:"swZmWD0MEXOm+pR9Xf3vP674doW9HgLxK8ntVyhUmrKsWc9GoCeX5Q3PgvVhkQ2lOA8GXZ1tXdrx6syfF5DZqBEiBASYyozL/KOd8C2r7oKIXeS1S6JzdwWiTxSD+PBpiMwBEbxj91tLKjrvcTCfzByCfNZdYv+/mi5Dxs/0wS6rdj62m5diUTy5MuY1NwC5E0OKShie0olyikDTflSUNMmiYjmykKW0xuXKJXsdOCO2DsqPCihRN2CeHsWXZZRJc8mPWPfqBElVGzJGzOpGDT4sfi8c9EuwFPsjUafB/2yrb2es5IsueSg/scqGmAxYZ+DpUO3OCioXr8z/zt21PA=="},
+            {alg:"SHA3224",sig:"UkOMw6DQHfALpkP6ChWYTPQmj/80YWL6DpkUJHaFQWhXFxuU/9JgpQ3qC6N/qZTHTwlpVlV+19gLrfhC2JJjCmNEM1zyuDd2Tln8Ny8MHK+aF4UD8JDOqQCn39uR6CLXpSHVFXP8RRzWo3YbmLI5WMZL087aKjcMQU7Yc7ebVFFcNJZrz7d8ZsSzUrTvxpuBypxDJT+Z2HfFDNQAty/pi1B6FetC3vbB/CWRXCk7NEqgZHavd/VsE4GixaM+PcQtVFjPRNTZir0aZfLey9hoYFOQ5oYVLM/RtmCTwQzRrcjUZv9EiPlZWqvokl1fFXD0ENRpDEFZ1Or2cqtV0zqdOg=="},
+            {alg:"SHA3256",sig:"idpbJpjm3Yz2qgiqNuicDK4cR3VMzu12ny40lSGwAln5w38KJhWdjjlyHwEjDHg1hXKSNRwCrIeykPuIIP3Wn+8mo/CRBUgBHahuCb0YwdfeE0CjeAKsIWoR+VmY36Eeocbc+zCOKMfw6Ybs2BvVZlycJ3R3YkzrL6+fQqKyhtWjg/CgrIBZ0g7Uq0U2HR9vD94vFwdBT63Xcf1tRc5Wq1TaxgsfNLY7SPaHwdNwJvvi01YhCe5W2HCf7dpzP/hn5mxxdOSIfi+j1wtBy4v5jezCB7Nzf45ZQ7vdCmGP1SKjAHsTSdJxuQBMACselLOniDdCscIIAhTPx6cOdM6ZyQ=="},
+            {alg:"SHA3384",sig:"L1TR8izeM0yRnMX7l6eKonHNGUmAsQi7GMOOmFQTFNZg9omVYYlgzCwq1LmR1R8DPAN40ZN9V0peEkocFT7zhLWLKAnE5YXBrm1iwMsIBK3WKLt2p7aknz1UwlGRZWlTlpPoOryYA0rgX4v8PnfYMjixck9Y5nPbl8+x/clnVzWEpCdeFw5wWFzskeFnXjAYaVVB0jtY3m8pKj+ZxCdU3sRtL3Ev8+wtRmW+h9BEicWYu98NqTAMH2TlGz/S3sq5IXl0XDau1ahwEj4QXp+B87JyR290yx87yi6KFcux7zkc+bfge9+pjZjDXO5LBao2jqvAuiDJ9PW96eUbbIq+XQ=="},
+            {alg:"SHA3512",sig:"pvPNCpOhu3Ju0VRR0tW1rwb5E6Px39xKZQVxKxdNrboJKhUmkDxEq8mm1EynbS8KshsB5YsbGmSpaDX+THKxlllDl0gHWJu54MV+Bh/HegvdorkrbPBzivFdiLj/ecY6Vc1HMcUd9mGvYlQ//fcOKvs8oGOK1Z2TE2ojKyuC/8Bsx/M29uRnBRUIh9nXD7eSAiui92NkNfCXjes7RLed6jO/kYbQw7Tl6h1hWoYHCBU39qJTK0NKV8U4/wI9A7tGDXfEbA51ulYS0XaYOUf7EqT/Aq+GV5w7CxOsc8P2B0h6YVLzUUHSia7CB6DYWkhk/v3ZC5qHxT92VOnC9715ag=="},
+        ]
+
         const sig = `let sig = fromBase64String("OXVKJwtSoenRmwizPtpjh3sCNmOpU1tnXUnyzl+PEI1P9Rx20GkxkIXlysFT2WdbPn/HsfGMwGJW7YhrVkDXy4uAQxUxSgQouvfZoqGSPp1NtM8iVJOGyKiepgB3GxRzQsev2G8Ik47eNkEDVQa47ct9j198Wvnkf88yjSkK0KxR057MWAi20ipNLirW4ZHDAf1giv68mniKfKxsPWahOA/7JYkv18sxcsISQqRXM8nGI1UuSLt9ER7kIzyAk2mgPCiVlj0hoPGUytmbiUqvEM4QaJfCpR0wVO4f/fob6jwKkGT6wbtia+5xCD7bESIHH8ISDrdexZ01QyNP2r4enw==")`
         const algs = ['NOALG', 'MD5', 'SHA1',
             // 'SHA224',//todo uncomment when implemented in ts-lib-crypto
@@ -144,10 +159,10 @@ func bar() = WriteSet([])`;
         await evaluate(pk)
         await evaluate(msg)
         await evaluate(sig)
-        algs.forEach((alg) => {
-            const rsaVerify = `rsaVerify(${alg}, msg, sig, pk)`
+        signatures.forEach(({alg, sig}) => {
+            const rsaVerify = `rsaVerify(${alg}, msg, fromBase64String("${sig}"), pk)`
             evaluate(rsaVerify).then(res => {
-                console.error(res)
+                console.error(`alg: ${alg}, res: ${JSON.stringify(res)}`)
                 expect('result' in res).toEqual(true)
             })
         })

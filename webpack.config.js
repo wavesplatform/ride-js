65366e95cff1d929627561c7898c84d53fef789f
Sergey Nazarov <phearnot@renee.ru>
2026-03-02 20:19:46 +0300

    fixed module layout

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path')
-const copy = require('copy-webpack-plugin')
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
-
 
 module.exports = {
   mode: 'production',
